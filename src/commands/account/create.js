a9d469891a08862ee8c118bfdf0d94b42c3b20d1
Brian Faust <hello@brianfaust.me>
2018-01-09 10:37:24 +0200

    implement account create method

--- src/commands/account/create.js
+++ src/commands/account/create.js
@@ -1,7 +1,12 @@
 const chalk = require('chalk')
+const arkjs = require('arkjs')
 
 const create = () => {
-  console.log(chalk.blue('Hello World!'))
+  const passphrase = require("bip39").generateMnemonic();
+
+  console.log(chalk.blue("Seed    - private:"), passphrase);
+  console.log(chalk.blue("WIF     - private:"), arkjs.crypto.getKeys(passphrase).toWIF());
+  console.log(chalk.blue("Address - public :"), arkjs.crypto.getAddress(arkjs.crypto.getKeys(passphrase).publicKey));
 }
 
 module.exports = {
